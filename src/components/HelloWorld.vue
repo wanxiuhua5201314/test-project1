bf6ff179e5ff1b6cc0fd937b06bbf54b102ff0b6
wanxiuhua <wanxiuhua@kedacom.com>
2019-02-22 15:57:03 +0800

    上传vue脚手架生成的初始项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <div>一起去过的地方</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
