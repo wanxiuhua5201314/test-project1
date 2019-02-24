ad3d4e536d7a948363055a64d29622ae4d6f20bd
wanxiuhua5201314 <15800922956@163.com>
2019-02-24 20:33:13 +0800

    周末修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="hello">
-    <div>小小企企鹅儿的小小世界</div>
+    <el-container class='top-container'>
+      <el-header  class="el-header"></el-header>
+       <el-main  class="el-main">
+         <el-input class="e-input" placeholder="请输入内容"></el-input>
+       <el-button style="margin-left:-5px" type="danger">搜索</el-button>
+       </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -16,5 +22,30 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style lang="scss" >
+.hello{
+  margin:0;
+  padding:0;
+  .top-container{
+     height: 100%;
+     border:1px solid;
+   }
+    .el-header{
+      background-color:lightgray
+
+    }
+    .el-main{
+      text-align: center
+    }
+    .el-button{
+      border-radius:0px
+    }
+    .e-input  .el-input__inner{
+      border-radius: 0px !important;
+    }
+    .e-input{
+      border-radius: 0px;
+      width: 30%
+    }
+    }
 </style>
