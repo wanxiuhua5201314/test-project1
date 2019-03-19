0c731f106138917e1e43c73e1cf653d3e6b0194c
wanxiuhua <wanxiuhua@kedacom.com>
2019-03-19 10:52:52 +0800

    测试v-show与v-if

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,18 +7,28 @@
        <el-button style="margin-left:-5px" type="danger">搜索</el-button>
        </el-main>
     </el-container>
+    <el-button type="primary" @click="test">测试v-if与v-show</el-button>
+    <div v-if="trueT">v-if</div>
+    <div v-show="trueT">v-show</div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      trueT: false
     }
   },
   methods: {
+    test () {
+      if (this.trueT === false) {
+        this.trueT = true
+      } else {
+        this.trueT = false
+      }
+    }
   }
 }
 </script>
